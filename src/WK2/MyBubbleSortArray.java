e523cc42ba191d44eaddc59372999fd05678b3f3
Jeremy <remote>
2016-08-31 14:38:49 -0400

    Completed Bubble sort algorithm.
    Commented out tools for using Arrays.sort()

--- src/WK2/MyBubbleSortArray.java
+++ src/WK2/MyBubbleSortArray.java
@@ -8,34 +8,67 @@ public class MyBubbleSortArray {
 	public static Scanner usr = new Scanner(System.in);
 	public static String tempStr;
 	public static int tempInt;
+	public static boolean running = true;
 	
 	
 	public static void main(String[] args) {
-		
-		System.out.println("How many numbers would you like to generate?");
-		
-		tempStr = usr.nextLine();
-		errHandler();
-		int array[] = new int[tempInt];
-		
-		
-		for (int i = 0; i < tempInt; i++) {
-			array[i] = (int) (Math.random()*100+1);
-		}
-		
-		Arrays.sort(array);
-		
-		System.out.println("You have generated the following numbers.");
-		for (int i = 0; i < tempInt; i++) {
-			if (i < tempInt -1) {
-				System.out.print(array[i] + ", ");
+		while (running) {	
+			
+			System.out.println("How many numbers would you like to generate?");
+			
+			tempStr = usr.next();
+			errHandler();
+			int array[] = new int[tempInt];
+			
+			
+			for (int i = 0; i < tempInt; i++) {
+				array[i] = (int) (Math.random()*100+1);
 			}
-			else {
-				System.out.print(array[i] + ".");
+			/*
+			
+			Easy mode
+			
+			Arrays.sort(array);
+			
+			*/
+			
+			//Bubble mode
+			
+			int temp;
+			
+			for (int i = 0; i < tempInt; i++) {
+				for (int j = 0; j < tempInt-1; j++) {
+					
+					if (array[j]>array[j+1]){
+						temp = array[j+1];
+						array[j+1] = array[j];
+						array[j] = temp;
+					}
+				}
 			}
 			
-		}
-
+			System.out.println("You have generated the following numbers.");
+			for (int i = 0; i < tempInt; i++) {
+				if (i < tempInt -1) {
+					System.out.print(array[i] + ", ");
+				}
+				else {
+					System.out.print(array[i] + ". \n\n");
+				}
+				
+			}
+			
+			System.out.println("Would you like to generate a new set of numbers?");
+			System.out.println("1 - Yes");
+			System.out.println("2 - No");
+			
+			tempStr = usr.next();
+			errHandler();
+			
+			if (tempInt == 2) {
+				running = false;
+			}
+		}	
 	}
 	
 	public static void errHandler() {
